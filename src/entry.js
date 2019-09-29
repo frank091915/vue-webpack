55559b823c3cfb2c554896df1dfe73de9a053cc9
frank091915 <frank091915@163.com>
2019-09-29 10:42:15 +0800

    首次推送

---
+++ src/entry.js
@@ -0,0 +1,21 @@
+// vue文件不能直接加载到html中，放入js中处理
+import Vue from "vue"
+import App from "./app.vue"
+
+import "./css/index.css"
+import "./css/stylus.styl"
+import "./assets/images/vueJs.jpg"
+
+// 初始化一个vue实例，然后调用方法，创建一个节点，插入html中，将vue渲染出来的dom挂在到该节点中
+var mountDom=document.createElement('div')
+document.body.append(mountDom)
+
+var backgroudDom=document.createElement('div');
+backgroudDom.setAttribute("id","mountDom")
+document.body.append(backgroudDom)
+
+console.log('lalal',process.env.NODE_ENV)
+
+new Vue({
+  render:(h)=>h(App)
+}).$mount(mountDom)
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path=require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack=require('webpack')
+const htmlPlugin=require('html-webpack-plugin')
+
+// path是node.js自带的模块
+
+// !!用于判断环境
+const isDev = process.env.NODE_ENV === "development"
+console.log(process.env.NODE_ENV)
+
+const config = {
+  target:'web',
+  // 为webpack指定一个目标打包环境（默认是web）,还可以是electron/node环境等
+  mode: 'development',
+  entry: path.join(__dirname,"src/entry.js"),
+  // path.join用来拼接路径，__dirname表示文件所在的根目录
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname,"dist")
+  },
+  // webpack原生只支持jsEs5,所以编译vue的话，需要loader来辅助
+  module:{
+    rules:[
+      {
+        test:/\.vue$/,
+        loader:'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+        // css-loader将css代码编译，而style-loader将编译后的代码放入网页中，而webpack加载顺序是从右到左，所以style-loader放在右边
+      },
+      {
+        test: /\.styl$/,
+        use: ['style-loader','css-loader',"stylus-loader"]
+      },
+      {
+        test:/\.(gif|jpeg|png|svg|jpg)$/,
+        // 正则表达式中，空格符也会算进去，所以匹配的时候注意不要留多余的空格符
+        use:[
+          {
+            options: {
+              // limit:1024,
+              name:'dist/[name]_frank.[ext]'
+            },
+            loader: "file-loader",
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new webpack.DefinePlugin({
+      'process.env':{
+        NODE_ENV : isDev ? '"development"' : '"production"'
+      }
+    }),
+    new VueLoaderPlugin(),
+    new htmlPlugin({
+      hash: true
+    })
+//     将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。例如，如果你有一条匹配 /\.js$/ 的规则，那么它会应用到 .vue 文件里的 <script> 块。
+  ],
+}
+
+// 如果process.NODE_ENV是否为development,修改config
+if(isDev){
+  config.devServer = {
+    port:'8088',
+    host:"0.0.0.0",
+    // 配置为0.0.0.0可以通过127.0.0.1和ip和localhost来访问
+    overlay:{
+      error:true,
+    },
+    hot:true,
+    // 这个选项开启后可以webpack打包后，遇到错误直接在页面上打印出来
+  }
+  config.plugins.push(
+    new webpack.HashedModuleIdsPlugin()
+  )
+  config.devtool='#cheap-nodule-eval-souce-map'
+}
+console.log(process.env.NODE_ENV)
+
+module.exports = config;
